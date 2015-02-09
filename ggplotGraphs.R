84d8760cb5f24b067f48dca0b7db651b1c748202
Anthony Williams <af_williams@outlook.com>
2015-02-08 19:18:04 -0800

    cleaned up ggplot graphs

--- ggplotGraphs.R
+++ ggplotGraphs.R
@@ -1,16 +1,20 @@
 # needs clean.df from consoleMunge.R
 
 library(ggplot2)
+#aes( x = substr(var, 1, 8)) trims the x labels!
+#theme(axis.title.x = element_blank() makes bad label go away!)
+
 
 #bar plot of players that killed player, filled with weapon
-#need to figure out how to trim axis label to make more compact
-p <- ggplot(subset(clean.df, subset = killed == "Bilbo T. Baggins"), aes(x = killer))
-p + geom_bar(aes(fill = weapon)) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p <- ggplot(subset(clean.df, subset = killed == "Bilbo T. Baggins"), 
+            aes(x = substr(killer, 1, 8)))
+p + geom_bar(aes(fill = weapon)) +
+  theme(axis.title.x = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
 
 #barplot kills by player filled with weapon. 
-#bar labels too crowded
 # group weapons into class, sniper, rifle, pistol, etc
-q <- ggplot(clean.df, aes(x = killer))
-q + geom_bar(aes(fill = weapon))
+q <- ggplot(clean.df, aes(x = substr(killer, 1, 8)))
+q + geom_bar(aes(fill = weapon)) + 
+  theme(axis.title.x = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
 
 
